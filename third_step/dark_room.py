613b548d7c9c61e70ce845674d5b6b75dab83c60
alves-Moises <moisesinho_ra@hotmail.com>
2026-02-20 23:27:36 -0300

    update final stage

--- third_step/dark_room.py
+++ third_step/dark_room.py
@@ -1,2 +1,39 @@
-def final_stage():
-    ""
+def final_stage(Passed, Lifes, Items, Choice):
+
+
+    combination_list = {
+        "frente": {
+            "combination": {"lanterna", "chave"},
+            "result": {
+                3: "Excelente supremo",
+                2: "Excelente",
+                1: "Neutro"
+            }
+        },
+        "servico": {
+            "combination": {"chave", "corda"},
+            "result": {
+                3: "Perfeito supremo",
+                2: "Perfeito",
+                1: "Neutro"
+            }
+        },
+
+        "telhado": {
+            "combination": {"lanterna", "corda"},
+            "result": {
+                3: "Ninja supremo",
+                2: "Ninja",
+                1: "Neutro"
+            }
+
+        } 
+    }
+
+    if Items == combination_list[Choice]["combination"]:
+        result = combination_list[Choice]["result"][Lifes]
+    else:
+        result = "Ruim"
+        
+    print("Você chegou a uma sala escura e encontra um cofre")
+    print(f"Resultado final: {result}")
