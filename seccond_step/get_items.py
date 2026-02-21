cfa489a8014d0b8a884668d6d42877f094430c6f
alves-Moises <moisesinho_ra@hotmail.com>
2026-02-20 21:10:50 -0300

    refactor seccond step

---
+++ seccond_step/get_items.py
@@ -0,0 +1,25 @@
+def select_item():
+    items = {"lanterna", "chave", "corda"}
+    valid = False
+
+    while not valid:
+        valid = True
+        print("lanterna - chave - corda")
+        try:
+            # tratamento já prevenindo duplicidade
+            item_list = set(input("Selecione dois itens:").lower().split())
+
+        except:
+            print("Digite novamente")
+        else:
+            if(len(item_list) == 2):
+                valid = True
+
+            for item in item_list:
+                if(not(item in items)):
+                    print("Entrada inválida")
+                    valid = False
+                    break
+                
+    return item_list
+            
